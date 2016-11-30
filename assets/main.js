7a0fab1c3ac72c8b071533cacfdfa0ce310f3cbf
macintoshhelper <noreply@macintoshhelper.com>
2016-11-30 01:16:49 +0000

    Improved header and intro area

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,24 @@ document.getElementById("menu-icon").addEventListener("click", function(){
         item.classList.add('nav-toggle');
     }
 });
+window.addEventListener("scroll", function() {
+    navbarCheck();
+});
+window.addEventListener("resize", function() {
+    navbarCheck();
+});
+
+
+// Functions
+//function navbarCheck() {
+
+//}
+function navbarCheck() {
+    var item = document.getElementById('site-header');
+    if (document.body.scrollTop > item.offsetHeight && !item.classList.contains('scrolled')) {
+        item.classList.add('scrolled');
+    }
+    else if (document.body.scrollTop <= item.offsetHeight && item.classList.contains('scrolled')) {
+        item.classList.remove('scrolled');
+    }
+};
